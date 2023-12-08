7e42d788601c9e247d978a39bbb7659b97ecdd2b
MouadSaber <148712620+MouadSaber@users.noreply.github.com>
2023-12-09 00:47:24 +0100

    Add files via upload

---
+++ tp_n3/tp_n3/Operations.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp_n3
+{
+    /// <summary>
+    /// La création d'un nouveau type de variable 'Operations'
+    /// les types d'opération peuvent réaliser par notre calculatrice
+    /// </summary>
+    public enum Operations
+    {
+        // L'ajout des valeurs
+        Ajouter,
+
+
+        // la différence des valeurs
+        Supp,
+
+        // La multiplication des valeurs 
+        Multip,
+
+        // La division des valeurs
+        Div,
+
+    }
+    
+}
